5a00d2016ce2ac808bd0be1fb883b08df4ea9417
Victor <victorlevesy@gmail.com>
2025-12-02 10:27:06 +0100

    add env var check in docker and start.sh

--- src/scraper/start.sh
+++ src/scraper/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+if [ -z "$ENVIRONMENT" ]; then
+    echo "ENVIRONMENT is not set. Please set it to dev or prod"
+    exit 1
+fi
 
-python -m src.scraper.api dev
+if [ "$ENVIRONMENT" = "dev" ]; then
+    python -m src.scraper.api dev
+else
+    if [ "$ENVIRONMENT" = "prod" ]; then
+        python -m src.scraper.api prod
+    else
+        echo "Invalid ENVIRONMENT"
+        exit 1
+    fi
+fi
